69170c668ca70ec962d76ee68d7399f190dc3759
Mongar28 <cristian.montoya.g@gmail.com>
2023-11-15 20:09:25 -0500

    segundo commit

---
+++ prueba1.py
@@ -0,0 +1,46 @@
+import time
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+# Registra el tiempo de inicio
+start_time = time.time()
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+sample = dataset[0]["audio"]
+
+result = pipe('profesor.mp3')
+print(result["text"])
+
+# Registra el tiempo de finalización
+end_time = time.time()
+
+# Calcula la diferencia de tiempo
+execution_time = end_time - start_time
+
+print(f"El script tomó {execution_time} segundos en ejecutarse.")
+
